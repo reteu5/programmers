5e1c038dcaf4ddfc871ad4753e5da671ee44fd4e
re.t <apeach5@kakao.com>
2024-01-18 16:17:04 +0900

    [chore] 전략 재수립

--- ring_bar_roll_graph.cpp
+++ ring_bar_roll_graph.cpp
@@ -1,6 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/258711
 // 도넛과 막대 그래프 Lv.02 [2024 KAKAO WINTER INTERNSHIP]
-
 #include <string>
 #include <vector>
 using namespace std;
@@ -9,44 +8,24 @@ const int arraySize = 1e6 + 1;
 int outbound[arraySize];
 int inbound[arraySize];
 
-class Graph {
-    private:
-        Node* node;
-        Node* root;
-
-    public:
-        Graph() {
-            this -> node = new Node(NULL, NULL);
-            this -> root = new Node(NULL, NULL);
-        }
-        void setRootNode(Node* node) { this -> root = node; }
-        Node* getRootNode() { return this -> root; };
-        bool isRing();  // 자기 자신으로 돌아오는지
-        bool isBar();   // 터미널 노드가 존재하는지
-        bool isRoll();  // 자기 자신으로 돌아오는지 AND 나가는 간선이 2개 이상인 노드 포함 여부
-};
-
-class Node {
-    private:
-        int id;
-        Node* nextNode = nullptr;
-        Node* prevNode = nullptr;
-    public:
-        Node(int id, int nextId) {
-            this -> id = id;
-            nextNode -> id = nextId;
-        }
-        void setNextNode(int id, Node* nextNode) {
-            this -> nextNode = nextNode;
-            nextNode -> prevNode = this;
-        };
-        int getId() { return this -> id; };
-        Node* getNextNode() { return this -> nextNode; };
-        Node* getPrevNode() { return this -> prevNode; };
-        int getNextNode(int id) { return nextNode -> getId(); }
-        int getPrevNode(int id) { return prevNode -> getId(); }
-};
-
+/*
+# 전략
+1. 루트 노드는 쉽게 찾을 수 있음.
+ - n(inbound) == 0 AND n(outbound) >= 2
+   - 이 조건 때문에 문제 조건에서 그래프 개수 총합은 2 이상으로 보장해주는 것.
+2. 이제 루트 노드에서 뻗어나가는 그래프는 식별할 수 있음.
+ - 각 그래프의 특성에 맞도록 알잘딱하게 식별만 하면 됨.
+3. 그래프 식별 전략
+ - 도넛 (Ring) : 무난쓰
+   - 총 개수에서 막대와 8자 제외한 만큼
+ - 막대 (Bar) : n(inbound) > 1 AND n(outbound) == 0인 Terminal Node 존재
+   - 알잘딱
+ - 8자 (Roll) : n(inbound) >= 2 AND n(outbound) == 2
+   - 알잘딱
+4. 그래프 별로 순회하지 말고, 특이 정점을 찾아내는 방식으로 작동하도록
+ - 어떻게든 그래프 순회해서 풀고 싶은데, 그래프 순회하면 인간적으로 너무 오래 걸림.
+ - 그래프 총 개수 2개 이상이라고 해준 걸 봐도 사실 그래프 순회하라고 낸 게 아니라 센스 보려고 낸 문제같은데, 이런게 입사해서 도움되는지는 잘 모?루
+*/
 
 void setVertexArray(vector<vector<int>> edges) {
     for (int i = 0; i < edges.size(); i++) {
@@ -57,77 +36,29 @@ void setVertexArray(vector<vector<int>> edges) {
     }
 }
 
-// n(inbound) == 0 AND n(outbound) > 2
-int findRootNode() {
-    for (int i = 0; i < arraySize; i++) {
-        if (inbound[i] == 0 && outbound[i] > 2) {
+int getRootNode() {
+    for (int i = 1; i < arraySize; i++) {
+        if (inbound[i] == 0 && outbound[i] >= 2) {
             return i;
         }
     }
     exit(-1);
 }
 
-vector<int> findConnectedGraphs(int rootNode, vector<int> edges) {
-    vector<int> graphs;
-    int bookmark = NULL;
-    // rootNode에서 출발하는 정점에 대하여 case 탐색 시작
-    for(int i = 0; i < arraySize; i++) {
-        if (edges[i][0] == rootNode) {
-            bookmark = edges[i][1];
-            // 그래프 유형을 판별하는 메서드 호출
-        }
-
-    }
-    graphs.push_back(rootNode);
-    for (int i = 0; i < edges.size(); i++) {
-        if (edges[i][0] == rootNode) {
-            graphs.push_back(edges[i][1]);
-        }
-    }
-    return graphs;
-}
-
-
-// int checkGraphType(int nominatedNode, vector<int> edges) {
-//     int caseFlag = NULL;
-//     // nominate부터 간선 따라 이동하면서 조건 충족하는지 체크
-//     for (int i = 0; i < edges.size(); i++) {
-//         if (edges[i][0] == nominatedNode) {
-//             caseFlag = edges[i][1];
-//             break;
-//         }
-//     }
-//     //caseFlag 값에 따라 switch case문으로 리턴값 분기
-// }
-
-
-void setMapping(vector<vector<int>> edges, Node* parentNode, int bookmarkStart = NULL, int bookmarkEnd = NULL) {
-    // edges를 탐색하며 parentNode에서 뻗어나가는 간선들을 mapping
-    for (int i = 0; i < edges.size(); i++) {
-        if (edges[i][0] == parentNode -> getId()) {
-            int nextId = edges[i][1];
-            Node* nextNode = new Node(nextId, NULL);
-            parentNode -> setNextNode(nextId, nextNode);
-            setMapping(edges, nextNode, bookmarkStart, bookmarkEnd);
-        }
-    }
-}
 
 
 
 
 vector<int> solution(vector<vector<int>> edges) {
+    // [생성한 정점의 번호, 도넛 모양 그래프의 수, 막대 모양 그래프의 수, 8자 모양 그래프의 수]
     vector<int> answer;
-    Graph graph;
-    Node* root;
+    int numOfTotalGraph = 0;
+    int numOfRing = 0;
+    int numOfBar = 0;
+    int numOfRoll = 0;
 
-    setVertexArray(edges);
-    int rootNodeId = findRootNode();
-    Node* rootNode = new Node(rootNodeId, NULL);
-    graph.setRootNode(rootNode);
-    Node* root = graph.getRootNode();
-
-    setMapping(edges, root);
+    answer[0] = getRootNode();
+    numOfTotalGraph = outbound[answer[0]];
 
     return answer;
 }
