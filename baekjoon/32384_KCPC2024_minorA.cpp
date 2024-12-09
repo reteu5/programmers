b98abce325ae9c825ac687d41ecfdd4897786e9f
reteu <apeach5@kakao.com>
2024-12-09 11:22:22 +0900

    ✨ feat: 고려대 KCPC2024 예비A- `사랑은 고려대입니다.` 문제 풀이. (브 5) 사랑은 고려대입니까?

---
+++ baekjoon/32384_KCPC2024_minorA.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    // Read number of nodes
+    cin >> N;
+
+    if (N > 0) 
+    {
+        cout << "LoveisKoreaUniversity";
+    }
+
+    for (int i = 1; i < N; i++) 
+    {
+        cout << " LoveisKoreaUniversity";        
+    }
+}
