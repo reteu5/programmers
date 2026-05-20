2920165d6ba41a2bfb78220f8cd1d2190af71c87
reteu <101304089+reteu5@users.noreply.github.com>
2026-05-20 20:53:35 +0900

    [level 1] Title: 공원 산책, Time: 0.04 ms, Memory: 4.23 MB -BaekjoonHub

--- programmers/1/172928.공원 산책/공원 산책.cpp
+++ programmers/1/172928.공원 산책/공원 산책.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <unordered_map>
 
 using namespace std;
 
@@ -9,81 +10,74 @@ vector<string> split(const string& input, char delimiter);
 vector<int> solution(vector<string> park, vector<string> routes) {
     vector<int> answer;
 
-    vector<pair<char, int>> parsedRoutes;
-
-    for (const string& route : routes) {
-        vector<string> tokens = split(route, ' ');
-
-        char direction = tokens[0][0];
-        int distance = stoi(tokens[1]);
-
-        parsedRoutes.push_back({direction, distance});
-    }
-
-    int row = park.size();
-    int col = park[0].size();
-
-    int curY = 0;
-    int curX = 0;
-
-    for (int y = 0; y < row; y++) {
-        for (int x = 0; x < col; x++) {
-            if (park[y][x] == 'S') {
-                curY = y;
-                curX = x;
+    int h = park.size();
+    int w = park[0].size();
+    
+    int x = 0;
+    int y = 0;
+    
+    // 출발지점 스캔
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            if (park[i][j] == 'S')
+            {
+                x = j;
+                y = i;
             }
         }
     }
+    
+    // Preset 처리
+    unordered_map<char, pair<int, int>> directions =
+    {
+        {'W', {-1, 0}},
+        {'E', {1, 0}},
+        {'N', {0, -1}},
+        {'S', {0, 1}}
+    };
+    
+    // 명령 실시간으로 처리하면서 이동
+    for (const string& route : routes)
+    {
+        vector<string> input = split(route, ' ');
+        char dir = input[0][0];
+        int dist = stoi(input[1]);
+        
+        int dx = directions[dir].first;
+        int dy = directions[dir].second;
+        
+        
+        
+        int nx = x;
+        int ny = y;
+        
+        bool valid = true;
 
-    for (const auto& route : parsedRoutes) {
-        char direction = route.first;
-        int distance = route.second;
+        for (int i = 0; i < dist; i++) {
+            nx += dx;
+            ny += dy;
 
-        int dy = 0;
-        int dx = 0;
-
-        if (direction == 'N') {
-            dy = -1;
-        } else if (direction == 'S') {
-            dy = 1;
-        } else if (direction == 'W') {
-            dx = -1;
-        } else if (direction == 'E') {
-            dx = 1;
-        }
-
-        int nextY = curY;
-        int nextX = curX;
-
-        bool canMove = true;
-
-        for (int step = 0; step < distance; step++) {
-            nextY += dy;
-            nextX += dx;
-
-            if (nextY < 0 || nextY >= row || nextX < 0 || nextX >= col) {
-                canMove = false;
-                break;
-            }
-
-            if (park[nextY][nextX] == 'X') {
-                canMove = false;
+            if (nx < 0 || nx >= w || ny < 0 || ny >= h || park[ny][nx] == 'X') {
+                valid = false;
                 break;
             }
         }
 
-        if (canMove) {
-            curY = nextY;
-            curX = nextX;
+        if (valid) {
+            x = nx;
+            y = ny;
         }
     }
-
-    answer.push_back(curY);
-    answer.push_back(curX);
-
-    return answer;
+    
+    return {y, x};
+    // return answer;
 }
 
+    
+    
+    
 vector<string> split(const string& input, char delimiter) {
     stringstream ss(input);
     string block;
