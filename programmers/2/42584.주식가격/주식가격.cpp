cbba13a78b04808c0d21d3d691865755411ccb01
reteu <101304089+reteu5@users.noreply.github.com>
2026-06-16 18:00:41 +0900

    [level 2] Title: 주식가격, Time: 22.40 ms, Memory: 26.6 MB -BaekjoonHub

---
+++ programmers/2/42584.주식가격/주식가격.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size(), 0);
+    
+    for (int i = 0; i < prices.size(); i++)
+    {
+        for (int j = i + 1; j < prices.size(); j++)
+        {
+            if (i == (prices.size() - 1)) continue;
+            if (prices[i] <= prices[j]) answer[i]++;
+            else
+            {
+                answer[i]++;
+                break;
+            }
+            
+        }
+    }
+    
+    return answer;
+}
